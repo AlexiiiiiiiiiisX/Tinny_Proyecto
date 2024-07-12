f6ce2c5042d62a06f054435b5ffc9b38fe8c60ef
AlexiiiiiiiiiisX <ayanadialla@gmail.com>
2024-07-12 10:38:41 -0500

    Primer subida

---
+++ secciones/Gestionar_encuestas.php
@@ -0,0 +1,20 @@
+<?php include ('../templates/cabecera.php'); ?>
+
+
+<div class="contenedor">
+  <div class="row">
+    <div class="col">
+      Column
+    </div>
+    <div class="col">
+      Column
+    </div>
+    <div class="col">
+      Column
+    </div>
+  </div>
+</div>
+
+
+
+<?php include ('../templates/pie.php'); ?>
